3e137fd29062c38147321b46cc7dc6067ba34a0b
Effie Guenther <effieguenther@admins-air.myfiosgateway.com>
2024-07-17 17:32:38 -0400

    header and banner

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,15 @@ module.exports = {
     './**/*.{vue,js,liquid}',
   ],
   theme: {
+    extend: {
+      colors: {
+        'lapisLazuli': '#2660A4',
+        'azure': '#EDF7F6',
+        'sandyBrown': '#F19953',
+        'copper': '#C47335',
+        'cafeNoir': '#56351E'
+      }
+    }
   },
   plugins: [
     formsPlugin,
