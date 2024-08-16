26bd4898248a003ec33ece42c88f1bf5c08d4d78
Effie <effie.guenther@tommyjohnwear.com>
2024-08-16 14:29:33 -0400

    Image Viewer uses AJAX API to obtain images

--- scripts/components/ImageViewer.vue
+++ scripts/components/ImageViewer.vue
@@ -1,16 +1,45 @@
 <template>
-  <p>Image Viewer</p>
+  <div v-if="productData">
+    <img v-if="productData" :src="activeImageSrc" :alt="productData.title" />
+    <div>
+    </div>
+  </div>
 </template>
 
 <script setup>
-  import { computed } from 'vue';
-  
+  import { ref, defineProps, onMounted, computed } from 'vue';
+
   const props = defineProps({
-    images: {
-      required: true,
+    product: {
+      required: true
     }
   })
 
-  console.log(props.images);
+  const productData = ref(null)
+  const idx = ref(0)
+
+  function increment() {
+    idx.value++
+  }
+
+  function decrement() {
+    idx.value--
+  }
+
+  const activeImageSrc = computed(() => productData?.value?.images[idx.value])
+
+  const fetchProduct = (() => {
+    fetch(window.Shopify.routes.root + `products/${props.product}.js`)
+    .then(response => response.json())
+    .then(product => {
+      productData.value = product
+      console.log(productData.value)
+    })
+    .catch(error => console.error(error))
+  })
+
+  onMounted(() => {
+    fetchProduct()
+  })
 
 </script>
