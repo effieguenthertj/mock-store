a1a1a237b7bd9f9c5f68d446b07a9037969a7dc9
Effie <effie.guenther@tommyjohnwear.com>
2024-08-16 15:07:02 -0400

    ImageViewer thumbnail scroller

--- scripts/components/ImageViewer.vue
+++ scripts/components/ImageViewer.vue
@@ -1,7 +1,24 @@
 <template>
   <div v-if="productData">
-    <img v-if="productData" :src="activeImageSrc" :alt="productData.title" />
-    <div>
+    <img v-if="productData" :src="activeImageSrc" :alt="productData.title" class="mb-2" />
+    <div class="relative">
+      <div ref="scrollContainer" class="flex relative overflow-x-hidden scroll-smooth">
+        <img 
+          v-for="(image, index) in productData.images"
+          @click="select(index)"
+          :key="`image_${index}`"
+          :src="image"
+          :alt="`${productData.title} image ${index}`"
+          class="mr-1 h-20 border-black"
+          :class="{ 'border-2': index === idx }"
+        />
+      </div>
+      <button @click="scrollLeft" class="h-full absolute bg-white opacity-75 px-2 top-0">
+        <
+      </button>
+      <button @click="scrollRight" class="h-full absolute bg-white opacity-75 px-2 top-0 right-0">
+        >
+      </button>
     </div>
   </div>
 </template>
@@ -17,13 +34,22 @@
 
   const productData = ref(null)
   const idx = ref(0)
+  const scrollContainer = ref(null);
 
-  function increment() {
-    idx.value++
+  function select(index) {
+    idx.value = index
   }
 
-  function decrement() {
-    idx.value--
+  function scrollLeft() {
+    if (scrollContainer.value) {
+      scrollContainer.value.scrollLeft -= 100
+    }
+  }
+
+  function scrollRight() {
+    if (scrollContainer.value) {
+      scrollContainer.value.scrollLeft += 100
+    }
   }
 
   const activeImageSrc = computed(() => productData?.value?.images[idx.value])
